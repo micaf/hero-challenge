63d142ca24745ffe04b53974609becf00515c8ed
mfuentes <mfuentes@airbiquity.com>
2024-11-05 15:35:57 -0300

    updated deprecated dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+import { loadingInterceptor } from './services/interceptor/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptors([loadingInterceptor])
+    ),
+    importProvidersFrom([
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
+    ]) ]
 };
