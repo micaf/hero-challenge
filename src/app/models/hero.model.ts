1c64e07887fdfb08442a9c584063b4f952e12423
mfuentes <mfuentes@airbiquity.com>
2024-11-06 02:42:09 -0300

    deleted unnecesary code and comments

--- src/app/models/hero.model.ts
+++ src/app/models/hero.model.ts
@@ -1,37 +1,10 @@
 export interface BaseHero {
-    /**
-     * Identificador único del héroe.
-     */
     id: number;
-
-    /**
-     * Nombre real del héroe.
-     */
     name: string;
-
-    /**
-     * Alias o nombre enmascarado del héroe.
-     */
     alias: string;
-
-    /**
-     * Descripción detallada del héroe.
-     */
     description: string;
-
-    /**
-     * Género del héroe.
-     */
     gender: string;
-
-    /**
-     * Lista de superpoderes del héroe.
-     */
     powersIds: number[];
-
-    /**
-     * IDs de los equipos a los que pertenece el héroe.
-     */
     teamIds?: number[];
 }
 
