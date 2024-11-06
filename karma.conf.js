95a113a37065570d45bb23577606ad225d230cdc
mfuentes <mfuentes@airbiquity.com>
2024-11-06 09:29:26 -0300

    deleted comments

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -14,14 +11,10 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
       },
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true 
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/hero-challenge'),
@@ -34,10 +27,11 @@ module.exports = function (config) {
         global: {
           statements: 80,
           functions: 80,
-          lines: 80
-        }
-      }
-    },
+          lines: 80,
+          branches: 0  
+        },
+      },
+      },
     reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
     restartOnFileChange: true
